dfc3612cb1218596c5468ca69dd8496e4886c892
busine011 <112756675+busine011@users.noreply.github.com>
2022-09-14 08:07:17 -0400

    Update BaseDatosMySql.php

--- BaseDatosMySql.php
+++ BaseDatosMySql.php
@@ -3,6 +3,6 @@
 $servidor = "mysql:dbname=apilink;host=localhost:3306"; 
 $usuario = "apilink"; 
 $pass = 'Geniunes12-';
-try{$pdo = new PDO($servidor, $usuario, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));}
+try{$pdo = new PDO($servidor, $usuario, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));}
 catch(PDOException $e){ echo "No se púede conectar" . $e->getMessage();}
 ?>
